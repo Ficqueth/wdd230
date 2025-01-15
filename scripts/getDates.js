f49f9d2c8ee6b2e5e1ca16c5829260aacfa288f1
helaman.ficquet <helaman.ficquet@cdw.com>
2025-01-15 14:52:27 -0600

    updated with hamburger menu

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -3,3 +3,11 @@ document.getElementById("year").textContent = new Date().getFullYear();
 
 // Dynamically set the last modified date in the second paragraph of the footer
 document.getElementById("lastModified").textContent = "Last modified: " + document.lastModified;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
